7a606ab79ec7b440b836919da7a27ff8010e9fe5
Mark Manipula <markmikomanipula@gmail.com>
2021-06-10 15:20:24 -0700

    added user input. need to work on validation next

--- miniproject02.py
+++ miniproject02.py
@@ -22,13 +22,14 @@ def filterByAttribute(position, attribute):
                         list.append(pokemon[1])
             print(list)
             
+            #asks the user if they want the printed list to be saved
+            save = input("Would you like to save this file? y / n ").lower()
             #creates the file
-            filename = f"fileteredBy{attribute}.txt"
-            with open(filename, "w") as outputFile:
-                  print(list, file = outputFile)
-
-filterByAttribute(2, "Ice")
-filterByAttribute(2, "Dragon")
+            if(save == "y"):
+                  filename = f"fileteredByType-{attribute}.txt"
+                  with open(filename, "w") as outputFile:
+                        print(list, file = outputFile)
+                        print("File saved!")
 
 #---------------------------------position--------------------------------------------
 #  1     2      3      4    5    6      7       8       9     10      11         12
@@ -44,11 +45,39 @@ def filterByStats(position, stats):
 
             print(list)
             
+            save = input("Would you like to save this file? y / n ").lower()
             #creates the file
-            filename = f"fileteredBy{position}.txt"
-            with open(filename, "w") as outputFile:
-                  print(list, file = outputFile)
+            if(save == "y"):
+                  filename = f"fileteredByStats.txt"
+                  with open(filename, "w") as outputFile:
+                        print(list, file = outputFile)
+                        print("File saved!")
 
+def main():
+      print("""
+            Welcome to Pokemon! What would you like to do?
+            1) Search Pokemon by type
+            2) See all Legendary Pokemon
+            3) Search powerful Pokemon
+            4) Quit program
+            """)
+      
+      userInput = input()
+      
+      if userInput == "1":
+            attribute = input("Please enter Pokemon type: ").title()
+            filterByAttribute(2, attribute)
+      elif userInput == "2":
+            print("Here is the list of all legendary Pokemon")
+            filterByAttribute(12, "True")
+      elif userInput == "3":
+            stats = int(input("Enter desired stats: "))
+            filterByStats(4, stats) 
+      elif userInput == "4":
+            print("Thanks for playing!")
+      else:
+            print("Invalid input. Try again")
+      
 
-filterByStats(4, 400)
-filterByStats(4, 600)
+if(__name__ == "__main__"):
+      main()
